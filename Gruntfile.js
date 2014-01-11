4f82063da33c9a5a68c38420e31a61b5be835409
Vladimir Feskov <v@vladimirfeskov.com>
2014-01-11 05:51:16 +0200

    added grunt task to update code, install any new dependencies and generate snapshots for search crawlers

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+var async = require('async'),
+    _ = require('lodash');
+
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.registerTask('update', 'update code, install any new dependencies, generate snapshots for search crawlers', function () {
+        var exec = require('child_process').exec,
+            callback = this.async(),
+            phantomJSParams = [
+                {
+                    url: 'http://localhost:3654',
+                    saveTo: './snapshots/index.html'
+                },
+                {
+                    url: 'http://localhost:3654/posts',
+                    saveTo: './snapshots/posts.html'
+                },
+                {
+                    url: 'http://localhost:3654/about',
+                    saveTo: './snapshots/about.html'
+                },
+                {
+                    url: 'http://localhost:3654/posts/file-upload-via-iframe-ajax-and-flash',
+                    saveTo: './snapshots/posts/file-upload-via-iframe-ajax-and-flash.html'
+                }
+            ],
+            snapshotCommands = _.map(phantomJSParams, function(params){
+                return 'phantomjs ./.phantomjs-runner.js ' + params.url + ' > ' + params.saveTo;
+            }),
+            commands = [
+                'git pull',
+                'npm install',
+                'bower install'
+            ].concat(snapshotCommands);
+
+        async.mapSeries(commands, function (command, next) {
+            grunt.log.writeln(command);
+            exec(command, next);
+        }, function (err, results) {
+            results.forEach(function (result) {
+                grunt.log.writeln(result);
+            });
+
+            if (err) {
+                grunt.log.error(err);
+                return callback(false);
+            }
+
+            return callback();
+        });
+    });
+};

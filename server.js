42d2d4bd2400132e9876a43d1112b6a6cccdf7fe
Vladimir Feskov <v@vladimirfeskov.com>
2014-01-11 03:12:38 +0200

    refactored node routing a little bit

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 var express = require('express.io'),
     app = express();
 
+app.use('/', function(){
+    var staticPath = __dirname + '/',
+        req = arguments[0];
 
-app.use('/', function(req, res){
-    var file
-    if(/_escaped_fragment_=/.test(req.url)){
-        file = req.url.replace(/\?.*$/,'');
-        if(file === '/'){
-            file += 'index.html';
-        } else {
-            file += '.html';
-        }
-        res.set('Content-Type', 'text/html').sendfile(__dirname + '/snapshots' + file);
-        return;
+    if(/_escaped_fragment_=/.test(req.url)){ // for google and bing crawlers
+        req.url = req.url.replace(/\?.*$/,'').replace(/\/+$/,'');
+        req.url += (req.url === '') ? '/index.html' : '.html';
+        staticPath += 'snapshots';
     } else {
-        express.static(__dirname + '/public').apply(this, arguments);
+        staticPath += 'public';
     }
+    express.static(staticPath).apply(this, arguments);
 });
 
 app.get('/[^\.]+$', function(req, res){
-    res.set('Content-Type', 'text/html').sendfile(__dirname + '/public/index.html');
+    res.set('Content-Type', 'text/html')
+        .sendfile(__dirname + '/public/index.html');
 });
 
 app.listen(process.env.PORT || 3654);

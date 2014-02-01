6029e92bbc8e1971ac2bab6b63b38891847b5b38
Vladimir Feskov <v@vladimirfeskov.com>
2014-02-01 16:10:22 +0200

    added missing components to custom bootstrap build

--- grunt/build/bootstrap.js
+++ grunt/build/bootstrap.js
@@ -1,4 +1,5 @@
 module.exports = [
     'bower_components/bootstrap/js/dropdown.js',
-    'bower_components/bootstrap/js/collapse.js'
+    'bower_components/bootstrap/js/collapse.js',
+    'bower_components/bootstrap/js/transition.js'
 ];
